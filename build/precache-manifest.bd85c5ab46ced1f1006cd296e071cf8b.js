1aa2592954ee67a9ee3f04427fcd2891551ff61c
willou <walid.boubguira@gmail.com>
2020-04-15 19:27:16 -0400

    'build'

---
+++ build/precache-manifest.bd85c5ab46ced1f1006cd296e071cf8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5b678ff275f7cd81283f725c3d6ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5144ff4f39014f8772c1",
+    "url": "/static/css/main.87690b62.chunk.css"
+  },
+  {
+    "revision": "e9bb455abd92300ce4d0",
+    "url": "/static/js/2.3bbb15a8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.3bbb15a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5144ff4f39014f8772c1",
+    "url": "/static/js/main.02d8854c.chunk.js"
+  },
+  {
+    "revision": "458b4f96ab9c97c67cef",
+    "url": "/static/js/runtime-main.36994c0b.js"
+  },
+  {
+    "revision": "18855f37e8d5904d5dee76e514adcda2",
+    "url": "/static/media/image2.18855f37.png"
+  }
+]);
--- src/api/index.js
+++ src/api/index.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async (country) => {
-  let changeableUrl = url;
+  let urlCho = url;
   if (country) {
-    changeableUrl = `${url}/countries/${country}`;
+    urlCho = `${url}/countries/${country}`;
   }
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(changeableUrl);
+    } = await axios.get(urlCho);
 
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (err) {
