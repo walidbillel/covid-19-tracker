c0eee61960d7aac677d27bf810271306047d0b16
willou <walid.boubguira@gmail.com>
2020-04-15 11:54:52 -0400

    covid init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
+
+export default App;
